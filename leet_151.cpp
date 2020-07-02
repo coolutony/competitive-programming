99fbd71ce43b1ae5df86b661f26788fa589839df
coolutony <coolutony@gmail.com>
2020-07-02 17:05:27 +0900

    Finish leet_151

--- leet_151.cpp
+++ leet_151.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <stack>
 #define MIN(X,Y) ((X) < (Y) ? : (X) : (Y))
 #define MAX(X,Y) ((X) > (Y) ? : (X) : (Y))
 using namespace std;
@@ -10,6 +11,32 @@ const int N=100001;
 
 int n,m;
 
+string reversewords(string s){
+	stack<char> st ;
+	string ans;
+	bool put_space = false;
+	for (int i = s.size()-1; i >= 0; i--){
+		char c = s[i];
+		if (c==' '){
+			while(!st.empty()){
+				ans.push_back(st.top());
+				st.pop();
+				put_space = true;
+			}
+			if(put_space){
+				ans.push_back(' ');
+				put_space = false;
+			}
+		}else{
+			st.push(c);
+		}
+	}
+	while(!st.empty()){
+		ans.push_back(st.top());
+		st.pop();
+	}
+	return ans;
+}
 int main(void) {
 	scanf("%d%d", &n, &m);
 }
