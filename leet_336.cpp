1aade4ea3339df2e1c27c60ace0d830538a15ffc
coolutony <coolutony@gmail.com>
2020-07-14 14:57:40 +0900

    leet_336.cpp failed first draft

--- leet_336.cpp
+++ leet_336.cpp
@@ -10,10 +10,21 @@ using namespace std;
 const int N=100001;
 
 int n,m;
-bool checkPalindrome(string s){
-	for(int i = 0; i < (s.size()+1)/2; i++){
-		if (s[i] != s[s.size()-1-i]) return false;
+bool checkPalindrome(string a, string b){
+	int i = 0;
+	int j = a.size() + b.size() -1;
+	while (i<j){
+		char c_i = ' ';
+		char c_j = ' ';
+		if (i<a.size()) {c_i = a[i];}
+		else {c_i = b[i-a.size()];}
+		if (j<a.size()) {c_j = a[j];}
+		else {c_j = b[j-a.size()];}
+		if(c_i != c_j) return false;
+		i ++;
+		j --;
 	}
+
 	return true;
 }
 vector<vector<int>> palindromePairs(vector<string> words){
@@ -21,7 +32,7 @@ vector<vector<int>> palindromePairs(vector<string> words){
 	for(int i = 0; i < words.size(); i++){
 		for (int j = 0; j< words.size(); j++){
 			if (i==j) continue;
-			if (checkPalindrome(words[i]+words[j])){
+			if (checkPalindrome(words[i],words[j])){
 				ans.push_back(vector<int>({i,j}));
 			}
 		}
