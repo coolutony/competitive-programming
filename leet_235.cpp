d3199f7731ae77b3dac2019e688b1916e316fa7b
Sunwook Kim <coolutony@gmail.com>
2020-08-21 10:58:59 -0400

    Complete leet_235

---
+++ leet_235.cpp
@@ -0,0 +1,12 @@
+#include <cstdio>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#define MIN(X,Y) ((X) < (Y) ? (X) : (Y))
+#define MAX(X,Y) ((X) > (Y) ? (X) : (Y))
+using namespace std;
+
+int main(void) {
+	int n,m;
+	scanf("%d%d", &n, &m);
+}
